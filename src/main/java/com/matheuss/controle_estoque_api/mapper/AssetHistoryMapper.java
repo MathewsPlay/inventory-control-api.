8b54f43238d3f13820c52eb8427efb11b669d532
MathewsPlay <mathewsplay16@gmail.com>
2026-01-22 23:00:24 -0300

    feat: Removido o campo supplier

--- src/main/java/com/matheuss/controle_estoque_api/mapper/AssetHistoryMapper.java
+++ src/main/java/com/matheuss/controle_estoque_api/mapper/AssetHistoryMapper.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface AssetHistoryMapper {
 
     @Mapping(source = "eventType.description", target = "eventType")
-    @Mapping(source = "associatedUser", target = "associatedUser")
     AssetHistoryResponseDTO toResponseDTO(AssetHistory assetHistory);
 
     List<AssetHistoryResponseDTO> toResponseDTOList(List<AssetHistory> assetHistoryList);
